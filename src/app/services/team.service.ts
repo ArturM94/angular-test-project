31aabac5b986b498b94fd2193923592327216066
Artur Manukian <manukian.artur.94@gmail.com>
2019-04-01 16:21:08 +0300

    Refactor TeamService

--- src/app/services/team.service.ts
+++ src/app/services/team.service.ts
@@ -1,25 +1,34 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+
+import { Teammate, TeammateAdapter } from '../models/teammate';
+import { RandomUserResponse } from '../interfaces/random-user-response';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TeamService {
 
-  constructor(private http: HttpClient) { }
+  private baseUrl = 'https://randomuser.me/api';
+  private teamSize = 9;
 
-  getTeam() {
-    return this.http.get<Teammates>('https://randomuser.me/api/?lego&results=9')
-      .pipe(map(response => response.results));
+  constructor(
+    private http: HttpClient,
+    private adapter: TeammateAdapter,
+  ) { }
+
+  getTeam(): Observable<Teammate[]> {
+    return this.http.get(`${this.baseUrl}/?lego&results=${this.teamSize}`).pipe(
+      map((data: RandomUserResponse) => data.results.map(item => this.adapter.adapt(item)))
+    );
   }
 
-  getTeammate(teammateId) {
-    return this.http.get<Teammates>(`https://randomuser.me/api/?lego&id=${teammateId}`)
-      .pipe(map(response => response.results[0]));
+  getTeammate(teammateId): Observable<Teammate> {
+    return this.http.get(`${this.baseUrl}/?lego&id=${teammateId}`).pipe(
+      map((data: RandomUserResponse) => data.results[0]),
+      map(item => this.adapter.adapt(item))
+    );
   }
-}
-
-export interface Teammates {
-  results: object;
 }
